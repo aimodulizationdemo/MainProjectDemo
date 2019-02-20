441804e1e91b497efbe9601f4c455814b5b03d6e
Master <>
2019-02-20 19:52:52 +0800

    init commit

---
+++ MainProjectDemo/MainProjectDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MainProjectDemo
+//
+//  Created by aizexin on 2019/2/20.
+//  Copyright © 2019 master. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
